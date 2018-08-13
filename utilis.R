d372bf01e2492add9a5a156e013f6b54e0287b42
pbotter42 <pdbotter@gmail.com>
2018-08-13 07:31:44 -0600

    added fullTest to utilis... appears to work.

--- utilis.R
+++ utilis.R
@@ -13,6 +13,7 @@ library(dplyr)
 # USS_mtr
 # uncollapsed_wEAP_cal #
 # rpPatternExtract #
+# fullTest #
 ##
 
 ### normal_distribution ###
@@ -161,4 +162,81 @@ USS_mtr <- function(theta,
   return(marginalTestReliability)
 }
 
-###
+### fullTest ###
+fullTest <- function(theta,
+                     thr,
+                     sl,
+                     wt) {
+  
+  nItems = length(thr);
+  nQuad = length(theta);
+  
+  allTlines = calculateTraceLines(theta = theta,
+                                  thr = thr,
+                                  sl = sl,
+                                  wt = wt,
+                                  nItems = nItems,
+                                  nQuad = nQuad)
+  
+  lwFinalIter = lw1(allTlines = allTlines,
+                    nItems = nItems)[[1]]
+  
+  # numeric matrix of only liklihoods
+  likMat <- matrix(nrow = length(lwFinalIter),
+                   ncol = length(theta))
+  
+  for(j in 1:length(lwFinalIter)) {
+    likMat[j,] <- lwFinalIter[[j]][[1]]
+  }
+  
+  #aggregateScoresOut <- aggregateScores2(lwFinalIter)
+  #summedScores <- aggregateScoresOut[["summedScores"]]
+  #allScores <- aggregateScoresOut[["allScores"]]
+  
+  # Numver of Quadrature points     
+  nQuad <- 21     
+  # theta range      
+  thetaMin <- -5     
+  thetaMax <- 5    
+  
+  qPoints <- seq(from = thetaMin,     
+                 to = thetaMax,     
+                 by = (thetaMax-thetaMin)/(nQuad-1))     
+  
+  distSim <- normal_distribution(nQuad,     
+                                 thetaMin,     
+                                 thetaMax)  
+  
+  #weights <- as.matrix(summedScores[,1])
+  #withOutWeights <- as.matrix(summedScores[,-1])
+  
+  posterior <- calculatePosterior(summedTraceLines = likMat,
+                                  dist = distSim)
+  
+  probabilities <- rowSums(posterior)
+  
+  # calculate wEAP... will try to calculate in arma later
+  wEAP <- numeric()
+  for(i in 1:length(probabilities)) {
+    wEAP[i] <- sum((posterior[i,]*qPoints)/probabilities[i])
+  }
+  # calculate SE and item marginal info
+  SE <- numeric()
+  itemMarginalInfo <- numeric()
+  for(i in 1:length(probabilities)) {
+    SE[i] <- sqrt(sum(posterior[i,]*(qPoints - wEAP[i])^2)/probabilities[i])
+    itemMarginalInfo[i] <- 1 - SE[i]^2
+  }
+  
+  marginalTestReliability <- 1 - sum(probabilities*SE)^2
+  
+  # calculating Item information
+  itemInfo = calculateInformation(traceLines = allTlines,
+                                  slope = sl,
+                                  qPoints = qPoints)
+  
+  imr <- rowSums(itemInfo) # calculating item marginal info
+  
+  
+  return(marginalTestReliability)
+}
